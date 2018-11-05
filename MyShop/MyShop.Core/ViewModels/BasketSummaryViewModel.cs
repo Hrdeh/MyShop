c395d36594a7c26a02abfc55695637bcd2022135
Hrdeh <40323956+Hrdeh@users.noreply.github.com>
2018-11-05 14:41:33 -0800

    basket service done

---
+++ MyShop/MyShop.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+	public class BasketSummaryViewModel
+	{
+		public int BastCount { get; set; }
+		public decimal BasketTotal { get; set; }
+
+		public BasketSummaryViewModel()
+		{
+
+		}
+
+		public BasketSummaryViewModel(int basketCount, decimal basketTotal)
+		{
+			this.BasketTotal = basketTotal;
+			this.BastCount = basketCount;
+		}
+	}
+}
